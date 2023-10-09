b2db3611c191ac35c884e3d637901ebb0152ea14
eviv2206 <zhenya.saprin@mail.ru>
2023-10-09 23:26:00 +0300

    minor update

--- src/main/java/by/brandwatches/authorizationserver/AuthorizationServerApplication.java
+++ src/main/java/by/brandwatches/authorizationserver/AuthorizationServerApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
+
 @EntityScan("by.brandwatches.authorizationserver.repository")
 @SpringBootApplication
 public class AuthorizationServerApplication {
